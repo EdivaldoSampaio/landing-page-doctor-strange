81a3a6b651769d8ec2e24f7be01153fa4cf243a8
edivaldosampaio <aldovsampaio@gmail.com>
2024-04-14 15:31:30 +0100

    terminando o projeto

--- js/burger.js
+++ js/burger.js
@@ -7,3 +7,5 @@ menu.addEventListener('click', active)
 
 
 // esta função é referente ao active, o javascript faz o menu burger de (=) virar o (X) ao clicar nele, ele está agindo emcima das configurações impostas no CSS do menu burger.
+
+//O addEventListener é o comando que faz com que o javascript escute o "click" e ativa o "active", por vez o active é uma função com o arrow function para o menu.classList.toggle, o mesmo realiza a troca do menu burger (=) para o (X), tirando o active, ao ser clicado pelo usuário no site.
